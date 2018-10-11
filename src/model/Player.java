54553442ee05d505f9bbed70b3a21118c4365184
Drazen Lucic <lucicd@hotmail.com>
2018-10-11 05:42:02 +0400

    Add Player class, 10-Oct-18

---
+++ src/model/Player.java
@@ -0,0 +1,38 @@
+/*
+ * The Player class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Gail Lee, Carolyn Murray, Drazen Lucic
+ */
+package model;
+
+/**
+ *
+ * @author Drazen
+ */
+public class Player {
+
+    public Player() {
+    }
+    
+        private String name;
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
