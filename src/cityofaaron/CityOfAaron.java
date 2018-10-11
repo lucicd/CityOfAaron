46000894bde9bf4538b165080819bd1f054715b4
Drazen Lucic <lucicd@hotmail.com>
2018-10-11 05:18:36 +0400

    Initial commit, 10-Oct-18

---
+++ src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Gail Lee, Carolyn Murray, Drazen Lucic
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author Drazen
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
