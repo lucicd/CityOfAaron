f542394695c3093aad4114f5535948abe30131d2
lucicd <lucicd@hotmail.com>
2018-10-24 07:52:12 +0400

    CropControl class, buyLand(), unit testing, 24-Oct-18

--- test/control/CropControlTest.java
+++ test/control/CropControlTest.java
@@ -5,6 +5,7 @@
  */
 package control;
 
+import model.CropData;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -22,12 +23,66 @@ public class CropControlTest {
      */
     
     @Test
-    public void testBuyLand() {
-        System.out.println("buyLand");
-        CropData cropData = null;
+    public void testBuyLandCase1() {
+        System.out.println("buyLand - Test Case 1");
+        CropData cropData = new CropData();
+        cropData.setWheatInStore(500);
+        cropData.setAcresOwned(2800);
+        int acresToBuy = 10;
+        int landPrice = 20;
+        int expResult = 2810;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testBuyLandCase2() {
+        System.out.println("buyLand - Test Case 2");
+        CropData cropData = new CropData();
+        cropData.setWheatInStore(500);
+        cropData.setAcresOwned(2800);
+        int acresToBuy = -5;
+        int landPrice = 20;
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testBuyLandCase3() {
+        System.out.println("buyLand - Test Case 3");
+        CropData cropData = new CropData();
+        cropData.setWheatInStore(500);
+        cropData.setAcresOwned(2800);
+        int acresToBuy = 100;
+        int landPrice = 20;
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testBuyLandCase4() {
+        System.out.println("buyLand - Test Case 4");
+        CropData cropData = new CropData();
+        cropData.setWheatInStore(500);
+        cropData.setAcresOwned(2800);
+        int acresToBuy = 25;
+        int landPrice = 20;
+        int expResult = 2825;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testBuyLandCase5() {
+        System.out.println("buyLand - Test Case 5");
+        CropData cropData = new CropData();
+        cropData.setWheatInStore(500);
+        cropData.setAcresOwned(2800);
         int acresToBuy = 0;
-        int landPrice = 0;
-        int expResult = 0;
+        int landPrice = 20;
+        int expResult = 2800;
         int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
         assertEquals(expResult, result);
     }
