0991aaac175428d55317f7c9243d49d913f4fe09
tweet <tweet@DESKTOP-F75UMBJ>
2018-11-13 20:09:26 -0700

    Adding inheritance to helpmenuview class 11/13/2018

--- src/view/HelpMenuView.java
+++ src/view/HelpMenuView.java
@@ -5,17 +5,12 @@
 //-------------------------------------------------------
 package view;
 
-import java.util.Scanner;
 /**
  *
  * @author tweet
  */
-public class HelpMenuView {
+public class HelpMenuView extends MenuView {
     
-    Scanner keyboard=new Scanner(System.in);
-    private final String theMenu;
-    private final int max;
-   
     // The HelpMenuView constructor
     // Purpose: Initialize the menu data
     // Parameters: none
@@ -23,7 +18,7 @@ public class HelpMenuView {
     // ===================================
 
     public HelpMenuView() {
-        theMenu = "\n" +
+        super("\n" +
                    "*****************************\n" +
                    "* CITY OF AARON: HELP MENU  *\n" +
                    "*****************************\n" +
@@ -32,35 +27,15 @@ public class HelpMenuView {
                    " 3 - How do I view the map?\n" +
                    " 4 - How do I move to another location?\n" +
                    " 5 - How do I display a list of animals, provisions and tools in the city storehouse?\n" +
-                   " 6 - Return to the Main menu\n";
-        max = 6;
-    }
-  // The getMenuOption method
-    // Purpose: gets the user's input
-    // Parameters: none
-    // Returns: integer - the option selected
-    // ===================================       
-    public int getMenuOption()
-    {
-        int userInput=0;
-        do
-        {
-            // get user input from the keyboard
-            userInput = keyboard.nextInt();
-            // if it is not a valid value, output an error message
-            if(userInput < 1 || userInput > max){
-                System.out.println("Error: you must select 1,2,3,4,5 or 6");
-            }
-            // loop back to the top of the loop if input was not valid
-            // end loop
-        } while (userInput <1 || userInput > max);
-        return userInput;
+                   " 6 - Return to the Main menu\n",
+                6);
     }
     //The doAction method
     //Purpose: performs the selected action
     //Parameters;none
     //Returns:none
     //==================================================================
+    @Override
     public void doAction(int option){
         switch(option){
             case 1://Goals of the game
@@ -83,23 +58,6 @@ public class HelpMenuView {
         }
     }
     
-    public void displayMenuView()
-    {
-        int menuOption;
-        do
-        {
-            // Display the menu
-            System.out.println(theMenu);
-            
-            // Prompt the user and get the user’s input
-            menuOption=getMenuOption();
-            
-            // Perform the desired action
-             doAction(menuOption);
-        } while (menuOption != max);
-
-    }
-
     private void viewGoals() {
         System.out.println("viewGoals Option Selected");
     }
